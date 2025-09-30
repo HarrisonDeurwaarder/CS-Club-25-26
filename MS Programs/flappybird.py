ae23ef9e244c648409ec988bfbedf7d3172a5f2f
CentralLoaf <hdwater2@gmail.com>
2025-09-29 20:01:00 -0700

    Images; collision; angling

--- MS Programs/flappybird.py
+++ MS Programs/flappybird.py
@@ -1,16 +1,30 @@
 import pygame as pg
 import sys
 from random import randint
+import math
 
 
-WINDOW = (1000, 500)
-PIPE_GAP = 180
+WINDOW = (1800, 900)
+PIPE_GAP = 350
 PIPE_SPEED = 5
 
 # Colors
 BLUE = (190, 245, 245)
 BIRD = (100, 100, 100)
 PIPE = (0, 255, 0)
+
+# Images
+
+BIRD_SIZE = (75, 50)
+PIPE_SIZE = (100, 1500)
+
+bird_img = pg.image.load('MS Programs\FBImages\\bird.png')
+bird_img = pg.transform.scale(bird_img, BIRD_SIZE)
+bird_angle = 0
+
+pipe_img = pg.image.load('MS Programs\FBImages\\pipe.png')
+pipe_img = pg.transform.scale(pipe_img, PIPE_SIZE)
+pipe_mask = pg.mask.from_surface(pipe_img)
 
 
 class Pipe:
@@ -33,15 +47,11 @@ class Pipe:
         '''
         Draw the bird at the given position
         '''
-        print(self.pos)
-        # Draw the top pipe
-        pg.draw.rect(screen,
-                     PIPE,
-                     pg.Rect(self.pos, 0, 20, self.start))
         # Draw the bottom pipe
-        pg.draw.rect(screen,
-                     PIPE,
-                     pg.Rect(self.pos, self.start + PIPE_GAP, 20, WINDOW[1] - self.start + PIPE_GAP))
+        screen.blit(pipe_img, (self.pos, self.start + PIPE_GAP))
+        
+        # Draw the top pipe
+        screen.blit(pg.transform.rotate(pipe_img, 180), (self.pos, self.start - PIPE_SIZE[1]))
     
 
 class Bird:
@@ -66,6 +76,9 @@ class Bird:
         '''
         # Position update
         self.pos -= self.vel
+        # Ensure the bird is in the bounds of the game
+        self.pos = 0 if self.pos < 0 else self.pos
+        self.pos = WINDOW[1] - BIRD_SIZE[1] if self.pos > WINDOW[1] - BIRD_SIZE[1] else self.pos
         # Velocity update
         self.vel -= gravity
          
@@ -74,10 +87,10 @@ class Bird:
         '''
         Draw the bird at the given position
         '''
-        pg.draw.circle(screen,
-                       BIRD,
-                       (100, self.pos),
-                       radius=20,)
+        screen.blit(
+            pg.transform.rotate(bird_img, math.atan(self.vel/10)*50), 
+            (100, self.pos),
+        )
     
     
 def main() -> None:
@@ -121,19 +134,29 @@ def main() -> None:
         bird.step()
         bird.draw(screen,)
         
+        # The mask checks for collisions
+        bird_mask = pg.mask.from_surface(pg.transform.rotate(bird_img, math.atan(bird.vel/10)*50))
+        
         for i, pipe in enumerate(pipes):
             # Check if the pipe is beyond the screen
-            if pipe.pos <= -10:
+            if pipe.pos <= -PIPE_SIZE[0]:
                 pipes.pop(i)
+                
             # Update the motion
             pipe.step()
             pipe.draw(screen,)
+            
+            # Check for collision
+            offset_top = (pipe.pos - 100, (pipe.start - PIPE_SIZE[1]) - bird.pos)
+            offset_bottom = (pipe.pos - 100, (pipe.start + PIPE_GAP) - bird.pos)
+            if bird_mask.overlap(pipe_mask, offset_bottom) or bird_mask.overlap(pipe_mask, offset_top):
+                main()
         
         # Update the display with all the changes made
         pg.display.flip()
         
-        # Limit update rate to 60fps
-        clock.tick(60)
+        # Limit update rate to 80fps
+        clock.tick(80)
         
         count += 1
         
